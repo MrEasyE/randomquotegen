c6065c49999dd422ee1d2967ba65e3d232b4084e
_MrEasyE_ <mreasye@protonmail.com>
2019-11-09 10:58:24 -0500

    Updates

---
+++ precache-manifest.e19edf5d9d1d1477a770b62e75f167ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58aac68cbf8ac0c50376a4aaafff954c",
+    "url": "/randomquotegen/index.html"
+  },
+  {
+    "revision": "87cf5b2353e0779fad65",
+    "url": "/randomquotegen/static/css/main.95f5f817.chunk.css"
+  },
+  {
+    "revision": "d19042b462a067a5a727",
+    "url": "/randomquotegen/static/js/2.906fd762.chunk.js"
+  },
+  {
+    "revision": "87cf5b2353e0779fad65",
+    "url": "/randomquotegen/static/js/main.962b0534.chunk.js"
+  },
+  {
+    "revision": "7a3acfdd3c50af70223e",
+    "url": "/randomquotegen/static/js/runtime-main.32acebf2.js"
+  }
+]);
